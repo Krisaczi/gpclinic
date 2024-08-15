9469efe5c1d71a4432e914593683882c5156cbf8
Krzysztof <krisaczi@yahoo.com>
2024-08-15 20:12:51 +0200

    landing page and login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultation";
 import FindDoctorSearch from "./Components/FindDoctorSearch/FindDoctorSearch";
+import SignUp from "./Components/Sign_Up/SignUp";
+import LandingPage from "./Components/LandingPage/LandingPage";
 
 function App() {
   return (
@@ -10,11 +12,13 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
+          <Route path="/landing-page" element={<LandingPage />} />
           <Route
             path="/instant-consultation"
             element={<InstantConsultation />}
           />
           <Route path="/find-doctor" element={<FindDoctorSearch />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <>
       <nav>
-        <div class="nav__logo">
+        <div className="nav__logo">
           <a href="/">
             StayHealthy
             <svg
@@ -29,32 +29,32 @@ const Navbar = () => {
           <span>.</span>
         </div>
 
-        <div class="nav__icon" onClick="{handleClick}">
-          <i class="fa fa-times fa fa-bars"></i>
+        <div className="nav__icon" onClick="{handleClick}">
+          <i className="fa fa-times fa fa-bars"></i>
         </div>
 
-        <ul class="nav__links active">
-          <li class="link">
-            <a href="../Landing_Page/LandingPage.html">Home</a>
+        <ul className="nav__links active">
+          <li className="link">
+            <a href="/landing-page">Home</a>
           </li>
 
-          <li class="link">
+          <li className="link">
             <a href="/find-doctor">Appointments</a>
           </li>
 
-          <li class="link">
+          <li className="link">
             <a href="/instant-consultation">Consultation</a>
           </li>
 
-          <li class="link">
-            <a href="../Sign_Up//Sign_Up.html">
+          <li className="link">
+            <a href="/sign-up">
               <button class="btn1">Sign Up</button>
             </a>
           </li>
 
-          <li class="link">
+          <li className="link">
             <a href="../Login/Login.html">
-              <button class="btn1">Login</button>
+              <button className="btn1">Login</button>
             </a>
           </li>
         </ul>
