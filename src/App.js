223a230fa6c69f091ab8394cd44d7a40f1a3c15d
Krzysztof <krisaczi@yahoo.com>
2024-08-19 19:30:25 +0200

    notifications

--- src/App.js
+++ src/App.js
@@ -6,22 +6,25 @@ import FindDoctorSearch from "./Components/FindDoctorSearch/FindDoctorSearch";
 import SignUp from "./Components/Sign_Up/SignUp";
 import LandingPage from "./Components/LandingPage/LandingPage";
 import Login from "./Components/Login/Login";
+import Notification from "./Components/Notification/Notification";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/landing-page" element={<LandingPage />} />
-          <Route
-            path="/instant-consultation"
-            element={<InstantConsultation />}
-          />
-          <Route path="/find-doctor" element={<FindDoctorSearch />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <Notification>
+          <Routes>
+            <Route path="/landing-page" element={<LandingPage />} />
+            <Route
+              path="/instant-consultation"
+              element={<InstantConsultation />}
+            />
+            <Route path="/find-doctor" element={<FindDoctorSearch />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Notification>
       </BrowserRouter>
     </div>
   );
